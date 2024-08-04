a3ced5faef01091fdcb8a50e822b88b2ea67a8fc
keerthanakrishnan124 <keerthanakrishnan124@gmail.com>
2024-08-04 16:12:13 +0530

    new commit

--- src/defaultPackage/Test.java
+++ src/defaultPackage/Test.java
@@ -3,8 +3,14 @@ package defaultPackage;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String word="yes";
+		
+		if(word=="yes") {
+			System.out.println("Yes");
+		}
+		else {
+			System.out.println("No");
+		}
 	}
 
 }
